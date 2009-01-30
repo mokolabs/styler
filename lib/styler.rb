d34823b229748590f3fa8958021d37084c506761
Patrick Crowley <patrick@mokolabs.com>
2009-01-30 08:19:16 -0800

    added support for IE8 stylesheet, removed line breaks from conditional comments (to keep things tight)

--- lib/styler.rb
+++ lib/styler.rb
@@ -17,7 +17,8 @@ module Styler
     stylesheets << "#{controller.controller_name}_#{controller.action_name}"
     stylesheets << "#{controller.controller_name}/#{controller.action_name}"
 
-    # IE6 / IE7
+    # IE6 / IE7 / IE8
+    stylesheets << "ie8"
     stylesheets << "ie7"
     stylesheets << "ie6"
 
@@ -25,10 +26,12 @@ module Styler
     stylesheets.collect! do |name| 
       if File.exist?("#{RAILS_ROOT}/public/stylesheets/#{name}.css")
         case name
+        when "ie8"
+          "<!--[if IE 8]>" + stylesheet_link_tag(name) + "<![endif]-->"
         when "ie7"
-          "<!--[if IE 7]>\n" + stylesheet_link_tag(name) + "\n<![endif]-->"
+          "<!--[if IE 7]>" + stylesheet_link_tag(name) + "<![endif]-->"
         when "ie6"
-          "<!--[if IE 6]>\n" + stylesheet_link_tag(name) + "\n<![endif]-->"
+          "<!--[if IE 6]>" + stylesheet_link_tag(name) + "<![endif]-->"
         else
           stylesheet_link_tag(name)
         end
